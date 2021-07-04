78b44620223b4e8a99f16b20fb96b91e3d4044e1
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-05 02:13:08 +0530

    Sign up

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const router = require("./src/routes/userRoutes");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ credentials: true }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Credentials", true);
+});
+
 app.use(router);
 
 connectDB
