e0d4506134114ff8b2a10c33fb91a03a0cac97a9
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-06 01:41:15 +0530

    Update userSignup.js

--- src/controllers/userSignup.js
+++ src/controllers/userSignup.js
@@ -19,7 +19,7 @@ const handleErrors = (err) => {
 
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.SECRET_KEY, {
+  return jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: maxAge,
   });
 };
