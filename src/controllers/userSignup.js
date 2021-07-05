78fd8961efa4970539f1709889fad1959871201b
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-05 15:30:38 +0530

    Update userSignup.js

--- src/controllers/userSignup.js
+++ src/controllers/userSignup.js
@@ -39,7 +39,7 @@ const signup = async (req, res) => {
     const token = createToken(user._id);
     res.cookie("jwt", token, {
       maxAge: maxAge * 1000,
-      httpOnly: true,
+      // httpOnly: true,
       // secure: true,
     });
     res.status(200).json({ user: user._id, token });
