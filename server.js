fa184b998daed8738073db17de6c26f68993fd50
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-08 19:00:37 +0530

    minor updates

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const cookieParser = require("cookie-parser");
 const connectDB = require("./src/helpers/connection");
 const router = require("./src/routes/userRoutes");
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser());
 
 const whitelist = ["https://proj-url-shortner.netlify.app"];
 const corsOptions = {
