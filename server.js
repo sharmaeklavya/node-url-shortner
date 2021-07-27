39baea8e24365e6da284194d5b0b7f2c58ae9fa6
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-27 16:35:53 +0530

    bug fixed

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(cors(corsOptions));
 app.use(function (req, res, next) {
   res.setHeader(
     "content-security-policy-report-only",
-    "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://5e52f4c893efcda6a7d40460.endpoint.csper.io"
+    "default-src 'self'; script-src 'self' 'report-sample'; style-src 'self' 'report-sample'; base-uri 'none'; object-src 'none'; report-uri https://node-mini.herokuapp.com"
   );
   next();
 });
