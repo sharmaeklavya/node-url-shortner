f896e61fb46de7ce2d1cfba2c8af5bef176a0452
eklavyasharma <24450132+sharmaeklavya@users.noreply.github.com>
2021-07-29 23:01:03 +0530

    bug fixed

--- src/models/emailer.js
+++ src/models/emailer.js
@@ -24,7 +24,7 @@ const emailer = async (email) => {
       subject: "Password Reset - Confirm your email",
       html: `<div style="margin : 0 auto; width: 400px; padding:1rem; text-align:center;">
       <h1 style="font-size: 1.5rem; color: tomato;">Mini URLs</h1>
-      <h2 style="font-size:1rem; color:salmon; text-align: left;"> <lead>Dear user,</lead> <br/> <br/>if you have requested for a new password. Please verify this email to reset the password or simply ignore this email.</h2>
+      <h2 style="font-size:1rem; color: rgba(31, 41, 55); text-align: left;"> <lead>Dear user,</lead> <br/> <br/>if you have requested for a new password. Please verify this email to reset the password or simply ignore this email.</h2>
       <div style="padding:1rem; margin:0.75rem auto; width:400px; height: 300px;">
       <a href="https://proj-url-shortner.netlify.app/update-password/${randomStr}">
         <img src="https://images.unsplash.com/photo-1590280986931-d675a2cf9b2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1047&q=80" style="width:100%; height:100%; text-decoration:none; cursor: pointer;" alt="recover-password">
